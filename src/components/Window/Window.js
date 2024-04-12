6bad545c6df1389e72ec7e250fc7b7d20f4918f0
guanch92 <guancheng92@gmail.com>
2024-04-13 03:33:12 +0800

    add window content

--- src/components/Window/Window.js
+++ src/components/Window/Window.js
@@ -1,5 +1,17 @@
+import { useSelector } from "react-redux";
 import "./Window.css";
 
 export function Window() {
-  return <div className="thx-window">Window</div>;
+  const { profiles, selectedId, editSelected } = useSelector(
+    (state) => state.profiles
+  );
+  const { textInput } = useSelector((state) => state.appState);
+
+  const { label } = profiles.find(({ id }) => id === selectedId) || {};
+  const title = editSelected ? textInput : label;
+  return (
+    <div className="thx-window">
+      <h1>{title}</h1>
+    </div>
+  );
 }
