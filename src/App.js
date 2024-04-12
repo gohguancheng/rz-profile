b32aa3d2772b7c2a74541391b0a996cad35ac625
guanch92 <guancheng92@gmail.com>
2024-04-12 22:51:17 +0800

    add fonts and html viewport alignment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import "./fonts/razerf5.css";
+import "./fonts/roboto.css";
 import { AppContainer } from "./components/AppContainer/AppContainer";
 import { Drawer } from "./components/Drawer/Drawer";
 import { Window } from "./components/Window/Window";
