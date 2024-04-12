1e278dea8b7d9589c478e117413bac8f2805dd4a
guanch92 <guancheng92@gmail.com>
2024-04-13 00:45:27 +0800

    test styles for list items with temporary mock values and states

--- src/components/Drawer/ProfileList/ListItem.js
+++ src/components/Drawer/ProfileList/ListItem.js
@@ -1,10 +1,30 @@
-import { memo } from "react";
+import { memo, useEffect, useRef, useState } from "react";
 import "./ListItem.css";
 
-const ListItem = memo(function ListItem({ label, type, isActive }) {
+const ListItem = memo(function ListItem({ label, type, isActive, editable }) {
+  const inputRef = useRef(null);
+
+  const [input, setInput] = useState(label);
+
+  useEffect(() => {
+    if (editable) {
+      inputRef.current.focus();
+    }
+  }, [editable]);
+
   return (
     <li className={`profile-item ${type} ${isActive ? "active" : ""}`}>
       {label}
+      {editable && (
+        <input
+          ref={inputRef}
+          className="profile-item"
+          type="text"
+          value={input}
+          onFocus={(event) => event.target.select()}
+          onChange={(evt) => setInput(evt.target.value)}
+        />
+      )}
     </li>
   );
 });
--- src/components/Drawer/ProfileList/ProfileList.js
+++ src/components/Drawer/ProfileList/ProfileList.js
@@ -4,31 +4,58 @@ import "./ProfileList.css";
 export function ProfileList() {
   const items = [
     {
+      id: 1,
       label: "Movie",
       type: "movie",
     },
     {
+      id: 2,
       label: "Game",
       type: "game",
     },
     {
+      id: 3,
       label: "Default",
       type: "default",
     },
     {
+      id: 4,
       label: "Music",
       type: "music",
     },
     {
+      id: 5,
       label: "Really Really Long Long Name",
       type: "custom",
       isActive: true,
+      editable: true,
+    },
+    {
+      id: 6,
+      label: "Really Really Long Long Name",
+      type: "custom",
+    },
+    {
+      id: 7,
+      label: "Really Really Long Long Name",
+      type: "custom",
+    },
+    {
+      id: 8,
+      label: "Really Really Long Long Name",
+      type: "custom",
     },
   ];
   return (
     <ul className="profile-list scrollable">
-      {items.map(({ label, type, isActive }) => (
-        <ListItem label={label} type={type} isActive={isActive} />
+      {items.map(({ id, label, type, isActive, editable }) => (
+        <ListItem
+          key={`${id}`}
+          label={label}
+          type={type}
+          isActive={isActive}
+          editable={editable}
+        />
       ))}
     </ul>
   );
