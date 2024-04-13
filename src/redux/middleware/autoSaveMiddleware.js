482a9edc135ccf66afaf4044e4f63ab4e28e2288
guanch92 <guancheng92@gmail.com>
2024-04-13 13:57:55 +0800

    add api call with axios

--- src/redux/middleware/autoSaveMiddleware.js
+++ src/redux/middleware/autoSaveMiddleware.js
@@ -1,7 +1,8 @@
 import { localStorageKey } from "../../const";
+import axios from "axios";
 
 const writeActionSubTypes = ["moveUp", "moveDown", "add", "rename", "del"];
-const autoSaveMiddleware = (store) => (next) => (action) => {
+const autoSaveMiddleware = (store) => (next) => async (action) => {
   const result = next(action);
   const actionSubType = action.type.split("/")[1];
   if (writeActionSubTypes.includes(actionSubType)) {
@@ -22,6 +23,43 @@ const autoSaveMiddleware = (store) => (next) => (action) => {
   if (actionSubType === "setTimeoutRef" && action.payload === null) {
     const newState = store.getState();
     localStorage.setItem(localStorageKey, JSON.stringify(newState.profiles));
+
+    const accessKey = process.env.REACT_APP_APIKEY;
+    const binId = process.env.REACT_APP_BIN_ID || "661a1b9bacd3cb34a837a2a6"; // note: defaults will be deleted
+
+    try {
+      const bin = await axios.get(
+        `https://api.jsonbin.io/v3/b/${binId}?meta=false`,
+        {
+          headers: {
+            ["X-Access-Key"]: accessKey,
+          },
+        }
+      );
+
+      if (bin.data) {
+        const res = await axios.put(
+          `https://api.jsonbin.io/v3/b/${binId}?meta=false`,
+          {
+            profiles: newState.profiles.profiles,
+          },
+          {
+            headers: {
+              ["X-Access-Key"]: accessKey,
+              ["Content-Type"]: "application/json",
+            },
+          }
+        );
+        console.log(`view your data at https://api.jsonbin.io/v3/b/${binId}`);
+        if (!process.env.REACT_APP_BIN_ID) {
+          console.log(
+            "Note that this bin is shared and being updated by other users"
+          );
+        }
+      }
+    } catch (err) {
+      console.log(err.response.data.message);
+    }
   }
 
   return result;
